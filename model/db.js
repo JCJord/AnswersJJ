744ec1fc0751c0729dd4dada2f8018ff82eed2d2
julio cesar <julinnn4k@live.com>
2021-04-15 00:51:41 -0300

    heroku db

--- model/db.js
+++ model/db.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize");
-const connection = new Sequelize("jjanswers", "b46a39d790a321", "d0d5c7fd", {
-    host: "us-cdbr-east-03.cleardb.com",
-    dialect: "mysql",
-});
+const connection = new Sequelize(
+    "heroku_45c2d39eeaa6588",
+    "b46a39d790a321",
+    "d0d5c7fd",
+    {
+        host: "us-cdbr-east-03.cleardb.com",
+        dialect: "mysql",
+    }
+);
 
 module.exports = connection;
